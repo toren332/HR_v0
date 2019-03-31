a7d53d8859ad07daa91290e21fb6cbfe2b03417e
toren332 <toren332@gmail.com>
2019-04-01 01:39:34 +0300

    transfered profiles to api_v0

--- HR_v0/settings.py
+++ HR_v0/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'profiles.apps.ProfilesConfig',
+    'api_v0',
     'rest_framework',
     'django_extensions',
 ]
--- HR_v0/urls.py
+++ HR_v0/urls.py
@@ -1,28 +1,17 @@
 from django.contrib import admin
 from django.urls import path
-from profiles import views as profiles_views
+from api_v0 import urls
+from django.conf.urls import include, url
+
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 
-# ACCOUNTS BLOCK
-
-router.register(r'account', profiles_views.AccountViewSet, 'account')
-router.register('users', profiles_views.UserViewSet, basename='users')
-router.register('profiles', profiles_views.ProfileViewSet, basename='profiles')
-router.register('teachers', profiles_views.TeacherViewSet, basename='teachers')
-router.register('students', profiles_views.StudentViewSet, basename='students')
-router.register('clients', profiles_views.ClientViewSet, basename='clients')
-
-
-# OBJECTS BLOCK
-
-router.register('groups', profiles_views.GroupViewSet, basename='groups')
-router.register('lessons', profiles_views.LessonViewSet, basename='lessons')
 
 
 urlpatterns = router.urls
 
 urlpatterns += [
+    url(r'^api/v0/', include(urls)),
     path('admin/', admin.site.urls),
 ]
